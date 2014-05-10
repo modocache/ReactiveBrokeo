fa98ebe66c38c100a91855c541a1b67683cbac4b
Brian Gesiak <modocache@gmail.com>
2014-05-10 15:56:57 +0900

    :bomb: I done broke it.

---
+++ README.md
@@ -0,0 +1,8 @@
+# ReactiveBrokeo
+
+[![Build Status](https://travis-ci.org/modocache/ReactiveBrokeo.svg)](https://travis-ci.org/modocache/ReactiveBrokeo)
+
+See? Build error.
+
+The problem is in [ReactiveBrokeo/main.m](https://github.com/modocache/ReactiveBrokeo/blob/7b20e8f7960a11ee3f6f2f69e20787e23d30d6ba/ReactiveBrokeo/main.m#L13).
+
---
+++ ReactiveBrokeo/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  ReactiveBrokeo
+//
+
+#import <Foundation/Foundation.h>
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
+int main(int argc, const char * argv[]) {
+    RACSignal *signal = [[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [[RACSignal return:RACTuplePack(@YES, @NO)] subscribe:subscriber];
+        return nil;
+    }] reduceEach:^(NSNumber *yes, NSNumber *no) {
+        // Casting block as (id) sidesteps type error.
+        // https://github.com/ReactiveCocoa/ReactiveCocoa/issues/937#issuecomment-42732343
+        NSLog(@"Am I indecisive? Well, %@ and %@.", yes, no);
+    }];
+
+    [signal subscribeNext:^(id _) {}];
+
+    return 0;
+}
+
